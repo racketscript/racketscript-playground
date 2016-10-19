b356a02ccfffbe4b0a0ad6921c04f90a863be664
Vishesh Yadav <vishesh3y@gmail.com>
2016-10-18 23:49:18 -0400

    Reformat

--- compile/app.rkt
+++ compile/app.rkt
@@ -11,7 +11,7 @@
 (define child-process ($/require "child_process"))
 (define spawn ($ child-process 'spawn))
 
-(define console #js.global.console)
+(define console #js*.global.console)
 
 ;;-----------------------------------------------------------------------------
 (define PORT 8080)
@@ -26,7 +26,7 @@
     [racket-code
      (#js.console.log "Compiling request.")
 
-     (define rapture (spawn "rapture"
+     (define racks (spawn "racks"
                             [$/array "-n"
                                      "--js"
                                      "--js-beautify"
@@ -38,30 +38,30 @@
 
      ;; Write to process input stream, followed by closing it
      ;; so that we get output
-     (#js.rapture.stdin.write racket-code)
-     (#js.rapture.stdin.end)
+     (#js.racks.stdin.write racket-code)
+     (#js.racks.stdin.end)
 
-     (#js.rapture.stdout.on "data"
-         (λ (data)
-           (set-box! output (string-append (unbox output) data))))
+     (#js.racks.stdout.on "data"
+       (λ (data)
+         (set-box! output (string-append (unbox output) data))))
 
-     (#js.rapture.stderr.on "data"
-         (λ (data)
-           (set-box! err (string-append (unbox err) data))))
+     (#js.racks.stderr.on "data"
+       (λ (data)
+         (set-box! err (string-append (unbox err) data))))
 
-     (#js.rapture.on "error"
-         (λ (err)
-           (#js.res.send "Error invoking compiler.")))
+     (#js.racks.on "error"
+       (λ (err)
+         (#js.res.send "Error invoking compiler.")))
 
-     (#js.rapture.on "close"
-         (λ (code)
-           (cond
-             [(zero? code)
-              (#js.res.status 200)
-              (#js.res.send (unbox output))]
-             [else
-              (#js.res.status 500)
-              (#js.res.send (unbox err))])))]
+     (#js.racks.on "close"
+       (λ (code)
+         (cond
+           [(zero? code)
+            (#js.res.status 200)
+            (#js.res.send (unbox output))]
+           [else
+            (#js.res.status 500)
+            (#js.res.send (unbox err))])))]
     [else
      ($> (#js.res.status 400)
          (send "Bad Request"))]))
