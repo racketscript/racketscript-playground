333e37f000c918dcefe7acb10e0f8ce38442f861
Vishesh Yadav <vishesh3y@gmail.com>
2016-10-23 07:14:43 -0400

    examples: Add quicksort

---
+++ examples/quicksort.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (append lst-a lst-b)
+  (match lst-a
+    ['() lst-b]
+    [(cons hd tl) (cons hd (append tl lst-b))]))
+
+(define (partition cmp pivot lst)
+  (let loop ([lst lst]
+             [part-a '()]
+             [part-b '()])
+    (match lst
+      ['() (cons part-a part-b)]
+      [(cons hd tl) #:when (cmp hd pivot)
+       (loop tl (cons hd part-a) part-b)]
+      [(cons hd tl) (loop tl part-a (cons hd part-b))])))
+    
+(define (quicksort cmp lst)
+  (match lst
+    ['() '()]
+    [(cons hd tl)
+     (match-define (cons part-a part-b) (partition cmp hd tl))
+     (append (quicksort cmp part-a)
+             (cons hd (quicksort cmp part-b)))]))
+
+(displayln (quicksort < '(1 5 3 9 3 8 12 5 32 21 16 4 5 1)))
+(displayln (quicksort > '(1 5 3 9 3 8 12 5 32 21 16 4 5 1)))
